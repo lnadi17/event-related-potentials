53fe37baebcd87510ede9dbf0ac36b57966b7310
Luka Nadiradze <lnadi17@freeuni.edu.ge>
2025-09-08 19:23:42 +0400

    Refactor EEG data handling to ensure correct stream order and improve error handling

--- ExperimentData.py
+++ ExperimentData.py
@@ -9,23 +9,14 @@ class ExperimentData:
         info = self._xdf_data[0]['info']
         self.metadata = {
             "effective_sample_rate": info['effective_srate'],
-            # "subject": {
-            #     # "name": info['desc'][0]['subject'][0]['name'][0],
-            #     # "alertness": info['desc'][0]['subject'][0]['alertness'][0],
-            #     "id": info['desc'][0]['subject'][0]['id'][0],
-            #     "group": info['desc'][0]['subject'][0]['group'][0],
-            #     "session": info['desc'][0]['subject'][0]['session'][0]
-            # },
             "markers": list(set(self.marker_data)),
-            # "experiment_variant": info['desc'][0]['experiment'][0]['variant'][0],
             "original_filename": original_filename
         }
 
     def _read_eeg_data(self):
         # Read data of the first stream
         self.eeg_time = self._xdf_data[0]['time_stamps']
-        # self._time_offset = min(self.eeg_time)
-        self._time_offset = 0  # Assume already aligned
+        self._time_offset = min(self.eeg_time)
         self.eeg_time = self.eeg_time - self._time_offset
         self.eeg_data = self._xdf_data[0]['time_series'][:, :8]
         # self.accelerometer_data = self._xdf_data[0]['time_series'][:, 8:11]
@@ -42,8 +33,13 @@ class ExperimentData:
 
     def __init__(self, xdf_path):
         self._xdf_data = pyxdf.load_xdf(xdf_path)[0]
-        # self._xdf_data = pyxdf.load_xdf(xdf_path, synchronize_clocks=False, dejitter_timestamps=False)[0]
-        self._xdf_data[0], self._xdf_data[1] = self._xdf_data[1], self._xdf_data[0]  # Swap streams if needed
+        assert len(self._xdf_data) == 2, "Expected exactly 2 streams in the XDF file."
+        # Ensure first stream is EEG and second is markers
+        if self._xdf_data[0]['info']['type'][0] != 'Data':
+            if self._xdf_data[1]['info']['type'][0] == 'Data':
+                self._xdf_data[0], self._xdf_data[1] = self._xdf_data[1], self._xdf_data[0]
+            else:
+                raise ValueError("No EEG stream found in the XDF file.")
         self._read_eeg_data()
         self._read_marker_data()
         self._read_metadata(os.path.basename(xdf_path))
